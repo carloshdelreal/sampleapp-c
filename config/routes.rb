4dd27ff6430ac11f8e36fd84b6c684af880a6c70
Carlos Del Real <carloshdelreal@gmail.com>
2019-07-31 18:36:20 -0500

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
   get 'foo/bar'
   get 'foo/baz'
-  root 'application#hello'
+  root 'static_pages#home'
 end
