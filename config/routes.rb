f3df1dbb63de3913b7f0a92cf340e3dee34064fa
Carlos Del Real <carloshdelreal@gmail.com>
2019-08-02 17:03:39 -0500

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'users/new'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+  root 'static_pages#home'
   get 'foo/bar'
   get 'foo/baz'
-  root 'static_pages#home'
 end
