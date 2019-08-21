947066b357f0602e50c3241e92b8e1c3deb5725e
Carlos Del Real <carloshdelreal@gmail.com>
2019-08-21 14:59:04 -0500

    Add Activation Account Finished last line

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    
+    redirect_to root_url and return unless @user.activated
     #debugger
   end
 
