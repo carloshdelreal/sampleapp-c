1a56a6521c5b54434caa813cd4f06b7f2815f1bc
Carlos Del Real <carloshdelreal@gmail.com>
2019-08-21 16:28:04 -0500

    user emailer finished

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,7 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Account activation"
   end
 
+  
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
